f842a143ffa6c5224218d3bbb712c0115fd60c21
sethij <keval@Keval>
2017-01-05 19:36:09 +0530

    controller updated

--- src/main/java/com/grocery/controller/GroceryApi.java
+++ src/main/java/com/grocery/controller/GroceryApi.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import com.grocery.entity.Grocery;
 import com.grocery.service.GroceryService;
 
 @RestController
+@CrossOrigin(origins="http://localhost")
 @RequestMapping(value = "api/grocery")
 public class GroceryApi {
 
@@ -29,7 +31,7 @@ public class GroceryApi {
 
 		Collection<Grocery> groceryList = service.findAll();
 		if (groceryList == null)
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
 		return new ResponseEntity<Collection<Grocery>>(groceryList, HttpStatus.OK);
 
